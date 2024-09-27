7e09e7d082dbbc9266b21baa4f40c328e7f27c0a
kash8580 <kashish.singh1101@gmail.com>
2024-09-27 11:54:09 +0530

    updated files

--- test_Add.py
+++ test_Add.py
@@ -1,10 +1,10 @@
 import unittest
-from add import add
+from add import mul
 
 class TestAddFunction(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(mul(1, 2), 2)
 
 if __name__ == '__main__':
     unittest.main()
